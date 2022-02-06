8da44895ac4990b8913414b99d5cb69845f3eae7
Alexandre Sieg <sieg.alexandre@gmail.com>
2022-02-06 12:22:24 +0100

    Refacto project (Add API)

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const fs = require('fs');
+const {google} = require('googleapis');
+
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+app.use(cors());
+app.use(express.json());
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+app.post('/names', urlencodedParser, (req, res) => {
+  console.log(req.body);
+  io.emit('names', req.body);
+  res.sendStatus(200);
+});
+
+app.get('/calendars/:name', (req, res) => {
+  console.log(req.params.name);
+  const content = JSON.parse(fs.readFileSync('credentials.json'));
+  const {client_secret, client_id, redirect_uris} = content.web;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  fs.readFile(`${req.params.name.toLowerCase()}_token.json`, (err, token) => {
+    if (err) return res.sendStatus(500);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+    calendar.events.list({
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime'
+    }, (err, result) => {
+      if (err) return res.sendStatus(500);
+      const events = result.data.items;
+      return res.send(events);
+    });
+  });
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:4000');
+});
