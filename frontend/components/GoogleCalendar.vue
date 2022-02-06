07e4560a8238523b7fc7f0cdf927196a66f65a99
Alexandre Sieg <sieg.alexandre@gmail.com>
2022-02-06 18:43:31 +0100

    Set default API host to 127.0.0.1

--- frontend/components/GoogleCalendar.vue
+++ frontend/components/GoogleCalendar.vue
@@ -22,7 +22,7 @@
 //@ts-nocheck
 import Vue from "vue";
 import Event from "~/components/Event.vue";
-const API_URL = process.env.API_URL || "http://192.168.1.11:4000"
+const API_URL = process.env.API_URL || "http://127.0.0.1:4000"
 export default Vue.extend({
   components: {
     Event
